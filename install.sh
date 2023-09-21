8a375158ba669af9874b3210d9e6708ec4966e5f
MD Emon Bhuiyan <109236498+emonbhuiyan@users.noreply.github.com>
2023-09-21 14:50:40 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update package
+opkg update
+
+#install packages
+opkg install iptables iptables-mod-nat-extra redsocks
+
+#Then run this line
+service redsocks stop && service redsocks disable && cd /etc && wget https://github.com/emonbhuiyan/BDIX/raw/main/bdix.conf && cd /etc/init.d && wget https://github.com/emonbhuiyan/BDIX/raw/main/bdix && chmod +x /etc/init.d/bdix
+
+
+echo -e "Thanks for installing. Follow me for more updates. https://fb.me/emoncontact"
