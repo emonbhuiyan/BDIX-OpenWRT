e8c18cd2c193b02e1bb9594928acad58dfc5fb35
MD Emon Bhuiyan <109236498+emonbhuiyan@users.noreply.github.com>
2023-09-22 10:58:02 +0600

    Update haha.sh

--- haha.sh
+++ haha.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 opkg update
 opkg install iptables iptables-mod-nat-extra redsocks
-service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://gitlab.com/emonbhuiyan/bdix/-/raw/main/bdix.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://gitlab.com/emonbhuiyan/bdix/-/raw/main/bdix && chmod +x /etc/init.d/bdix
+service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://gitlab.com/emonbhuiyan/emon/-/raw/main/bdix.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://gitlab.com/emonbhuiyan/emon/-/raw/main/bdix && chmod +x /etc/init.d/bdix
